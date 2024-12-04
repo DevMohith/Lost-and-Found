b829b687eab852d42f9b066dedc6c8290f2c8459
Rohit Sardeshmukh <sardeshmukhrohit@gmail.com>
2024-12-04 21:53:42 +0100

    Revert "added login and register frontend"
    This reverts commit 467384e33a268315b0b67f034ec9515204c14546.

--- lostnfoundwebsite/src/main.js
+++ lostnfoundwebsite/src/main.js
@@ -1,10 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-<<<<<<< Updated upstream
 
 createApp(App).mount('#app')
-=======
-createApp(App).use(router).mount('#app');
->>>>>>> Stashed changes
